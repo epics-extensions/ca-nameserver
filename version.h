44c0ae5648a7bbe6937a515ac5ce2063d7e9cd56
Janet B. Anderson <jba@aps.anl.gov>
2005-06-03 12:44:19 -0500

    Initial version.

---
+++ version.h
@@ -0,0 +1,33 @@
+/*************************************************************************\
+* Copyright (c) 2002 The University of Chicago, as Operator of Argonne
+* National Laboratory.
+* Copyright (c) 2002 Southeastern Universities Research Association, as
+* Operator of Thomas Jefferson National Accelerator Facility.
+* Copyright (c) 2002 The Regents of the University of California, as
+* Operator of Los Alamos National Laboratory.
+* This file is distributed subject to a Software License Agreement found
+* in the file LICENSE that is included with this distribution. 
+\*************************************************************************/
+/* version.h */
+
+/************************DESCRIPTION***********************************
+ *  nameserver Version and Credits Information
+**********************************************************************/
+
+#ifndef INCversionh
+#define INCversionh 1
+
+#define NS_VERSION       2
+#define NS_REVISION      0
+#define NS_MODIFICATION  0
+#define NS_UPDATE_LEVEL  6
+
+
+#define NS_VERSION_STRING "Nameserver Version 2.0.0.6"
+
+#define NS_CREDITS_STRING  \
+          "Developed at Thomas Jefferson National Accelerator Facility\n" \
+          "and at Argonne National Laboratory \n\n" \
+          "Authors: Joan Sage and Janet Anderson\n\n"
+
+#endif /* INCversionh */
